16123281b286abb243af0ad8bc71e1c2e6f6c4fc
diajara1 <amazandiajara@gmail.com>
2026-08-09 19:33:52 +0800

    Create practice1.cs

---
+++ practice1.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the radius of the circle: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+
+        double area = Math.PI * radius * radius;
+
+        Console.WriteLine(area);
+    }
+}
